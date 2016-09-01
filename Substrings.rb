c434b57e28588d84af88361a1caefb0566b856ed
Michael J Orlando <michael.john.orlando@gmail.com>
2016-09-01 22:15:21 +0000

    bubble_sort

--- Substrings.rb
+++ Substrings.rb
@@ -1,7 +1,6 @@
 # a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) 
 # as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original 
 #string and how many times it was found.
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word,dictionary)
     @answer_part = []
@@ -26,4 +25,5 @@ def substrings(word,dictionary)
     puts ""
 end
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 substrings("Howdy partner, sit down! How's it going?", dictionary)
