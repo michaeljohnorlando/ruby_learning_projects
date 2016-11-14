f9f94513a5768673ff35788b6b20935b274fb8a3
Michael Orlando <michael.john.orlando@gmail.com>
2016-11-14 09:32:48 -0800

    linked list works... to str was incorperated all along (tested every method in the console

--- Linked_Lists.rb
+++ Linked_Lists.rb
@@ -1,4 +1,7 @@
-# random thoughts... @head == nil || @head.nil?      what looks better?
+# random thoughts...
+#@head == nil || @head.nil?      what looks better?
+#would make an each method for at index and size
+#ooh shouldn't have been using value the whole time...
 
 class LinkedList
   def initialize
@@ -42,21 +45,59 @@ class LinkedList
     return nil if @tail.nil?
     @tail.value
   end
-  def at(index)
+  def at(index) #returns the node at the given index
+	node = @head
+	return nil  if index > size || node.nil? || !index.is_a?(Integer)   # makes shure chosen index is valid
+	return node if index == 0                                     #takes care of the first index because the loop always looks at the next node
+	(index - 1).times do                                                      #runs through each node index times until hit
+	    node =  node.next_node
+    end
+	return node
+  end
+  def pop #removes the last element from the list... (just stops refenceing it...new tail)
+	@tail = at(size-1)
+  end
+  def contains?(value)#returns true if the passed in value is in the list and otherwise returns false
+	size_of_list = size
+	until size_of_list < 0
+	  return true if at(size_of_list).value == value
+	  size_of_list -= 1
+	end
+	return false
+  end
+  def find(data) #returns the index of the node containing data, or nil if not found
+    return nil if !contains?(data) # if the data isnt present then nil
+    size_of_list = size
+	until size_of_list < 0  #checking each value --"right" to "left"
+	  return size_of_list if at(size_of_list).value == data
+	  size_of_list -= 1
+	end
+  end
 end
 
-class Node # makes a new entry 
+class Node # makes a new entry
   attr_accessor :value, :next_node
   def initialize(value = nil, next_node = nil)
     @data      = value
     @next_node = next_node
   end
 end
+
 test = LinkedList.new
 test.append(2,'two')
 test.prepend(1,'one')
 test.prepend(0,'zero')
 test.append(3,'three')
-#puts test.head   <<= zero
-#puts test.tail   <<= three   
-#puts test.size   <<= 4
+#puts test.head               #<<= zero
+#puts test.tail               #<<= three   
+#puts test.size               #<<= 4
+#puts test.at(0).value        #<<= zero
+#puts test.at(3).value        #<<= three
+#puts test.at(4).value        #<<= nil
+#puts test.contains?('two')   #<<= true
+#puts test.contains?('blah')  #<<= false
+#puts test.find('two')        #<<= 3
+
+#puts test.tail   #<<= three
+#puts test.pop
+#puts test.tail   #<<= two
