96c18f48f13c7adc4f031ce77b0d201fb6265454
michaeljohnorlando <michael.john.orlando@gmail.com>
2017-07-14 01:17:28 -0400

    Sinatra Hangman functional...

--- Rails/Sinatra/hangman/hangman.rb
+++ Rails/Sinatra/hangman/hangman.rb
@@ -3,10 +3,13 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 class Hangman
-  attr_accessor :wrong_guess,:word
+  attr_accessor :wrong_guess,:word,:modified_word,:win,:info
   def initialize
-    @wrong_guess  = []
-    @word         = random_word_from_dictionary[0]
+    @wrong_guess   = []
+    @word          = random_word_from_dictionary[0].downcase
+    @modified_word = create_blank_word_display(@word)
+    @win           = false
+    @info          = false
   end
   def random_word_from_dictionary # needs to be > 5 letters
       CSV.read("./public/dictionary.csv").sample
@@ -18,26 +21,30 @@ class Hangman
       end
       return underscore_array_for_ammount_of_letters
   end
-  def letter_in_word(guess,word)
-      if guess[/[a-z]+/]  != guess
+  def letter_in_word(guess)
+    @info = false
+      if guess[/[a-z]+/]  != guess || guess.length != 1
+        @info = true
         return "that is not a valid guess"
       elsif
-        word.include? guess
+        @word.include? guess
         return true
       else
         @wrong_guess << guess
         return  false
       end
   end
-  def letter_position_in_word_display(letter,word,chosen_word)
-      if letter_in_word(letter,word) && letter.length == 1
-          until letter_in_word(letter,word) == false
-              @blank_word_display[word.index(letter)] = letter
-              @modified_word[word.index(letter)]      = ' '
-          end
-      elsif letter == chosen_word
-          @win =  true
+  def player_move(guess)
+    counter = 0
+    if letter_in_word(guess)
+      @word.each_char do |letter,index|
+        if letter == guess
+          @modified_word[counter] = letter
+        end
+        counter += 1
       end
+    end
+    @win = true if @modified_word.join == @word
   end
 end
 
@@ -45,20 +52,23 @@ end
 # Sinatra (Web brick) #
 #######################
 game   = Hangman.new
-status = game.create_blank_word_display(game.word)
 
 get "/" do
+  game   = Hangman.new if game.win
+  game   = Hangman.new if game.wrong_guess.count > 5
   guess  = params['guess'].to_s
-  game.letter_in_word(guess,game.word) if defined? guess
+  game.player_move(guess) if defined? guess
   erb :index, :locals => {
     :word       => game.word,
     :guess      => guess,
-    :status     => status,
+    :status     => game.modified_word,
     :wrong_guess  => game.wrong_guess,
+    :win_status   => game.win,
+    :info         => game.info,
   }
 end
 
-letter_position_in_word_display(gets.chomp,@modified_word,chosen_word)
+
 
 #https://stackoverflow.com/questions/40600428/why-is-an-array-of-my-rails-objects-showing-up-when-using-remote-true/40600471#40600471
 #http://www.integralist.co.uk/posts/sinatra.html
